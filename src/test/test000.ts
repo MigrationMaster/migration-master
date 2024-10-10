7cbf44eb27af1ea2145ce38d1524e44e3ae963ab
rupam2234 <rupam.bharali99@gmail.com>
2024-10-10 17:01:27 +0530

    Review DB Design

--- src/test/test000.ts
+++ src/test/test000.ts
@@ -7,7 +7,21 @@ export default async () => {
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
   );
   console.log(worker);
-  const op1 = 
+
+  const op1 = await worker.from("migrations_order").insert({
+    user_id: 0,
+    order_id: 12,
+    migration_title: "Test Migration 1",
+    comment: "first test here",
+    status: 1,
+  });
+  console.log(op1);
+
+  const order = await worker.from("order_list").insert({
+    order_id: 12,
+    user_id: 0,
+  });
+  console.log(order);
 
   const op = await worker.from("files").insert({
     order_id: 12,
