0304e7821eec96aac3953fdcae89e0e9ff563897
Vikramjit Borah <vikramjitborah@gmail.com>
2024-10-14 22:08:50 +0530

    clueless

---
+++ TODO.md
@@ -0,0 +1,7 @@
+<!-- 1. Order_id need to generate at order_list and then exist on migration -->
+
+<!-- Review DB Design -->
+
+# assign default email to every connected platforms
+
+##
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import React from "react";
 import "./globals.css";
-// import Dashboard from "@/components/UI/dashboard/dashboard";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const geistSans = localFont({
   src: "./(fonts)/GeistVF.woff",
@@ -25,14 +26,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <div></div>
-        {/* <Dashboard></Dashboard> */}
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <div></div>
+          {/* <Dashboard></Dashboard> */}
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
